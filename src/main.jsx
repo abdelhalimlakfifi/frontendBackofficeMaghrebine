f61721b891ae6243e8d0f60f5afb245acd734a97
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-31 12:54:44 +0100

    add primeReact provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
+import { PrimeReactProvider } from "primereact/api";
 import store from './store/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <PrimeReactProvider value={{ unstyled: true, pt: {}  }}>
     <Provider store={store}>
       <App />
     </Provider>
+  </PrimeReactProvider>
+    
 )
