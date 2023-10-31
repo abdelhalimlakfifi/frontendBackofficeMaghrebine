48c79ed268df21e21647256927a713377ed11827
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-31 12:56:57 +0100

    build SideImage component

---
+++ src/components/login/SideImage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import sideImage from "../../assets/sideImage";
+
+const sideImg = () => {
+    return (
+      <>
+       <div className="" style={{ width: "30%" }}>
+            <img
+              src={sideImage}
+              className="w-full h-full filter contrast-75"
+              alt="Login"
+            />
+          </div>
+      </>
+    )
+  }
+  
+  export default sideImg;
