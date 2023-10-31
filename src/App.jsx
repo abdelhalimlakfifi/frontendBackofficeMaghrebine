b0f2cee53445b191e9fe55ac47d51c52bbde7ab2
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-10-31 13:10:48 +0100

    update login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Login from './components/login/loginForm'
+import Login from './pages/Login'
 import Profile from './pages/Profile'
 import ProtectedRoute from './utils/ProtectedRoutes';
 
