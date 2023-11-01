b9e958c3e602e4645ed7ae9ad209de490581fd8e
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-02 00:08:47 +0100

    create button component

---
+++ src/components/confirmEmail/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Button } from "primereact/button";
+
+
+export default function Botton() {
+  return (
+    <>
+      <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
+        <div className="flex items-center ">
+        <Link to="/InsertCode">
+            <Button label="Request Password Reset" className="text-xm" />
+        </Link>        
+        </div>
+        
+      </div>
+      <div  className="flex items-center  ">
+          <Link to="/Login" className="w-full my-1 underline text-xs flex justify-center text-custom-purple hover:underline">Back To Sign In</Link>
+      </div>
+    </>
+  );
+}
