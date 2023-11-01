d00c7fd46356dc773beebf9ee87cfe7033d9ee93
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-01 23:02:36 +0100

    update style

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../store/UserSlice";
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import facebook from "../../assets/facebook.png"
+
 
 //import primeReact styles
 import { InputText } from "primereact/inputtext";
@@ -61,7 +62,7 @@ const LoginForm = () => {
             onChange={onChangeUsername}
             className="p-invalid block w-full px-4 my-[2rem] py-2 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
           />
-          <label htmlFor="username" className="pl-2">
+          <label htmlFor="username" className="">
             Username
           </label>
         </span>
@@ -76,7 +77,7 @@ const LoginForm = () => {
             onChange={onChangePassword}
             className="p-invalid block w-full px-4 py-2 my-5 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
           />
-          <label htmlFor="password" className="pl-2">
+          <label htmlFor="password" className="">
             Password
           </label>
         </span>
@@ -87,11 +88,11 @@ const LoginForm = () => {
       <Botton />
 
       <div className="w-full my-2 flex">
-        <a href="#" className="text-xs text-custom-purple hover:underline">
+        <Link to="/ConfirmEmail" className="text-xs text-custom-purple hover:underline">
           Forget Password?
-        </a>
+        </Link>
       </div>
-      <div className="w-full flex text-xs text-custom-purple hover:underline">
+      <div className="w-full flex text-xs text-custom-purple">
         You do not have an account?
         <a href="#" className="text-xs text-red-500 hover:underline">
           Create One.
