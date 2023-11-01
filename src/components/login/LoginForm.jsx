9592b55b67ac914b385954e9f5836d29646ae671
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-01 01:06:41 +0100

    add components to form

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../store/UserSlice";
 import { useNavigate } from "react-router-dom";
+import { Button } from "primereact/button";
+import facebook from "../../assets/facebook.png"
+
+
+
+
 
 //import styles
 import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
-import { ProgressSpinner } from 'primereact/progressspinner';
+
+//import components
+import Botton from "./Button";
 
 
-export default function LoginForm() {
+
+
+const LoginForm = () => {
   // react states
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -55,42 +64,64 @@ export default function LoginForm() {
             required
             value={username}
             onChange={onChangeUsername}
-            className="p-invalid block w-full px-4 my-[20px] py-2 text-custom-purple bg-white border rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
+            className="p-invalid block w-full px-4 my-[2rem] py-2 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
           />
-          <label htmlFor="username" className="pl-2">Username</label>
+          <label htmlFor="username" className="pl-2">
+            Username
+          </label>
         </span>
       </div>
 
       <div className="">
-        <span className="p-float-label  text-gray-500">
+        <span className="p-float-label text-gray-500">
           <InputText
             id="password"
             required
             value={password}
             onChange={onChangePassword}
-            className=" p-invalid block my-[30px] w-full px-4 py-2 text-custom-purple bg-white border rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
+            className="p-invalid block w-full px-4 py-2 my-5 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40"
           />
-          <label htmlFor="password" className="pl-2">Password</label>
+          <label htmlFor="password" className="pl-2">
+            Password
+          </label>
         </span>
       </div>
 
-      <a href="#" className="text-xs text-custom-purple hover:underline">
-        Forget Password?
-      </a>
+        {/* <Botton/> */}
 
-      {/* <button type="submit">{loading ? "Loading..." : "Login"}</button> */}
-      <div className="">
-        {loading ? (
-          <ProgressSpinner />
-        ) : (
-          <Button
-            label="Submit"
-            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple"
-          />
-        )}
+      <div className="w-full my-2 flex">
+        <a
+          href="#"
+          className="text-xs text-custom-purple hover:underline"
+        >
+          Forget Password?
+        </a>
+      </div> 
+      <div className="w-full flex text-xs text-custom-purple hover:underline">
+        You do not have an account?
+        <a
+          href="#"
+          className="text-xs text-red-500 hover:underline"
+        >
+          Create One.
+        </a>
+      </div> 
+
+      {/* {error && <div>{error}</div>} */}
+      
+     <Divider/>
+
+        
+     <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-custom-purple  bg-white rounded-md  border-2 focus:outline-none">
+        <div className="flex items-center ">
+          <Button label="Google" className="text-xl" />
+
+        </div>
       </div>
+      
 
-      {error && <div>{error}</div>}
     </form>
   );
-}
+};
+
+export default LoginForm;
