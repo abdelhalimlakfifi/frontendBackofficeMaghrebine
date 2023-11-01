8077a0d3f309588804451ae2b7eb6279d5cbcd6b
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-01 01:05:50 +0100

    create login button component

---
+++ src/components/login/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "primereact/button";
+
+// import { ProgressSpinner } from "primereact/progressspinner";
+import 'primeicons/primeicons.css';
+
+export default function Botton() {
+  return (
+    <>
+      <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
+        <div className="flex items-center ">
+          <Button label="Login" className="text-xl" />
+          <i className="pi pi-sign-in  ml-5"></i>
+        </div>
+      </div>
+    </>
+  );
+}
