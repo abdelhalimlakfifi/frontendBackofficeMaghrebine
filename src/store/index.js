cc1455a5c43b746b3e69de1183ee78c54f9d1585
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-01 23:00:32 +0100

    add forgotPasswordReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore } from "@reduxjs/toolkit";
 import userReducer from './UserSlice';
+import forgotPasswordReducer from './ForgotPasswordSlice'
+
 
 
 const store = configureStore({
     reducer:{
         user: userReducer,
+        forgotPassword: forgotPasswordReducer
     }
 })
 
 
-export default store
+export default store 
