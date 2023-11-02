62435681068b3ee2be827b1ac1306d6844d304c2
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-02 03:11:48 +0100

    update resetPassword path in button

--- src/components/InsertOtp/Button.jsx
+++ src/components/InsertOtp/Button.jsx
@@ -9,7 +9,7 @@ export default function Botton() {
     <>
       <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
         <div className="flex items-center ">
-        <Link to="/InsertCode">
+        <Link to="/ResetPassword">
             <Button label="Verify" className="text-xm" />
         </Link>        
         </div>
