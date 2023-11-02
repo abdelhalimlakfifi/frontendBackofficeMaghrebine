a437e4b5992307542d37cdb3f3ade55b71e75f12
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-02 03:09:56 +0100

    create otp validation slice

---
+++ src/store/ValidateOtpSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const validateOTP = createAsyncThunk('otp/validate', async (otp) => {
+    try {
+
+      const response = await axios.post('http://localhost:3000/api/validate-otp', { otp });
+      return response.data;
+
+    } catch (error) {
+      throw error;
+    }
+  });
+
+
+  const otpReducerSlice = createSlice({
+    name: 'otp',
+    
+    initialState: {
+        loading: false,
+        success: false,
+        error: null,
+    },
+
+    extraReducers: (builder) => {
+      builder
+        .addCase(validateOTP.pending, (state) => {
+          state.loading = true;
+          state.success = false;
+          state.error = null;
+        })
+        .addCase(validateOTP.fulfilled, (state) => {
+          state.loading = false;
+          state.success = true;
+          state.error = null;
+        })
+        .addCase(validateOTP.rejected, (state, action) => {
+          state.loading = false;
+          state.success = false;
+          state.error = action.error.message;
+        });
+    },
+  });
+  
+export default otpReducerSlice.reducer;
