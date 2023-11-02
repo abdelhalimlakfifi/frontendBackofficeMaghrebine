30ae72109bef1d4f0aadaefca1772fc5353d8819
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-02 01:34:45 +0100

    add confirmEmail and InsertOtp routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import ConfirmEmail from './pages/ConfirmEmail'
 import Profile from './pages/Profile'
 import ProtectedRoute from './utils/ProtectedRoutes';
+import InsertOtp from './pages/InsertOtp'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
                 <Routes>
                     <Route exact path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
+                    <Route path='/ConfirmEmail' element={<ConfirmEmail />} />
+                    <Route path='/InsertOtp' element={<InsertOtp />} />
                     <Route path='/profile' element={
                         <ProtectedRoute>
                             <Profile/>
