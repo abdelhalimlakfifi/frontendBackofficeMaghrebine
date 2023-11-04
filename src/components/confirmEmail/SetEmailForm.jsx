8eb35ee7a253e3804650f67d355e8fd2514c54fa
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-04 15:58:55 +0100

    refactor code

--- src/components/confirmEmail/SetEmailForm.jsx
+++ src/components/confirmEmail/SetEmailForm.jsx
@@ -13,64 +13,51 @@ import { InputText } from "primereact/inputtext";
 
 const SetEmailForm = () => {
 
-  const [email, setEmail] = useState("");
-  const { loading, success, error } = useSelector((state) => state.otp);
+    const [email, setEmail] = useState("");
+    const { loading, success, error } = useSelector((state) => state.otp);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
+    const onChangeEmail = (e) => {
+        const email = e.target.value;
+        setEmail(email);
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!loading) { // Check if not loading to prevent multiple submissions
+            dispatch(forgotPassword(email));
+            localStorage.setItem('userEmail', email); // Save the email to local storage to retreive it in the InsertOtp page
+        }
+    };
 
+    return (
+        <form className="" onSubmit={handleSubmit}>
+            <div className="">
+                <img 
+                    src={emailImage}
+                    className="w-full h-52 py-5 my-10"
+                    alt="Email"
+                />
+            </div>
 
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!loading) { // Check if not loading to prevent multiple submissions
-    dispatch(forgotPassword(email));
-    localStorage.setItem('userEmail', email); // Save the email to local storage to retreive it in the InsertOtp page
-    }
-  };
-  
-   
+            <div className="w-full my-7">
+                <p className=" text-base ">Forgot Your Password? </p>
+                <p className="text-xs">Please enter your email Adress. You will receive a 6 digits number to create a new password.</p>
+            </div>
 
-  
-  return (
-    <form className="" onSubmit={handleSubmit}>
-      <div className="">
-      <img 
-        src={emailImage}
-        className="w-full h-52 py-5 my-10"
-        alt="Email"
-      />
-      </div>
+            <span className="p-float-label text-gray-500">
+                <InputText id="email" value={email} onChange={onChangeEmail} className="p-invalid block w-full px-4 py-2 my-5 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40" />
+                <label htmlFor="email">Insert Your Email</label>
+            </span>
 
-      <div className="w-full my-7">
-          <p className=" text-base ">Forgot Your Password? </p>
-          <p className="text-xs">Please enter your email Adress. You will receive a 6 digits number to create a new password.</p>
-      </div>
+            {loading && <p>Loading...</p>}
+            {error && <p className="text-red-900">{error}</p>}
+            {success && <p>OTP Sent Successfuly </p>}
+            <Botton  type="submit" disabled={loading} />
 
-      <span className="p-float-label text-gray-500">
-        <InputText id="email" value={email} onChange={onChangeEmail} className="p-invalid block w-full px-4 py-2 my-5 text-custom-purple bg-white border-2 rounded-md focus:border-custom-purple focus:ring-custom-purple focus:outline-none focus:ring focus:ring-opacity-40" />
-        <label htmlFor="email">Insert Your Email</label>
-      </span>
-
-      
-
-      {loading && <p>Loading...</p>}
-
-      {error && <p className="text-red-900">{error}</p>}
-
-      {success && <p>OTP Sent Successfuly </p>}
-
-      <Botton  type="submit" disabled={loading} />
-
-      
-
-    </form>
-  );
+        </form>
+    );
 };
 
 export default SetEmailForm;
--- src/pages/ConfirmEmail.jsx
+++ src/pages/ConfirmEmail.jsx
@@ -8,22 +8,20 @@ import SetEmailForm from "../components/confirmEmail/SetEmailForm";
 
 function confirmEmail() {
     return (
-      <>
-          <div className="w-full flex ">
-              <div className=" w-full items-center flex">
-                  <div id="form" className=" mx-60 w-full">
-                      <Logo/>
-                      <SetEmailForm />
-                  </div>
-              </div>
-  
-              <div className="w-[48%]">
-                  <SideImg className="h-screen w-full"/>
-              </div>
-          </div>
-      </>
+        <>
+            <div className="w-full flex ">
+                <div className=" w-full items-center flex">
+                    <div id="form" className=" mx-60 w-full">
+                        <Logo/>
+                        <SetEmailForm />
+                    </div>
+                </div>
+
+                <div className="w-[48%]">
+                    <SideImg className="h-screen w-full"/>
+                </div>
+            </div>
+        </>
     );
-  }
-  
-  
-  export default confirmEmail
+}
+export default confirmEmail
