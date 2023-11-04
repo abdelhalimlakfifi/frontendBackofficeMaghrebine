88d5cbad5447abbce6b2c5f5ff93d716d44a8a9d
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-04 23:19:51 +0100

    create profile icon component

---
+++ src/components/navbar/ProfileIcon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import profilicon from "../../assets/ProfileIcon.svg";
+
+const ProfileIcon = () => {
+    return (
+      <>
+        <img
+          src={profilicon}
+          className=" "
+          alt="Profile"
+        />
+      </>
+    )
+  }
+  
+  export default ProfileIcon;
