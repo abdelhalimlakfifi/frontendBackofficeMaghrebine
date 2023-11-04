f2d3a52e84d9228e80c3052bfe62ce223997dd16
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-04 23:24:03 +0100

    create menu component

---
+++ src/components/navbar/MenuIcon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Menuicon from "../../assets/menu.svg";
+
+const MenuIcon = () => {
+    return (
+      <>
+        <img
+          src={Menuicon}
+          className="h-8"
+          alt="notification"
+        />
+      </>
+    )
+  }
+  
+export default MenuIcon;
