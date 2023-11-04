d023770f27993606a400e9d30a7e28f67fc98f77
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-04 23:23:31 +0100

    create navbar logo component

---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavLogo from "../../assets/NavLogo.svg";
+
+const NavbarLogo = () => {
+    return (
+      <>
+        <img
+          src={NavLogo}
+          className="h-8"
+          alt="notification"
+        />
+      </>
+    )
+  }
+  
+  export default NavbarLogo;
