46292c54a2e8272780945ca4e0a6538d5f7e2c11
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-05 15:38:14 +0100

    make the click submit the form

--- src/components/confirmEmail/Button.jsx
+++ src/components/confirmEmail/Button.jsx
@@ -5,19 +5,16 @@ import { Button } from "primereact/button";
 
 
 export default function Botton() {
-  return (
-    <>
-      <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
-        <div className="flex items-center ">
-        <Link to="/InsertOtp">
-            <Button label="Request Password Reset" className="text-xm" />
-        </Link>        
-        </div>
-        
-      </div>
-      <div  className="flex items-center  ">
-          <Link to="/Login" className="w-full my-1 underline text-xs flex justify-center text-custom-purple hover:underline">Back To Sign In</Link>
-      </div>
-    </>
-  );
+    return (
+            <>
+                <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
+                    <div className="flex items-center ">        
+                        <Button label="Request Password Reset" type="submit" className="text-xm" />
+                    </div>
+                </div>
+                <div  className="flex items-center  ">
+                    <Link to="/Login" className="w-full my-1 underline text-xs flex justify-center text-custom-purple hover:underline">Back To Sign In</Link>
+                </div>
+            </>
+    );
 }
