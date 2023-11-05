f6f620fbf2190a53beca0ee7957733f7c357ccc6
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-06 00:28:03 +0100

    make input return value to parrent

--- src/components/InsertOtp/OtpInputFields.jsx
+++ src/components/InsertOtp/OtpInputFields.jsx
@@ -3,23 +3,22 @@ import React, { useState } from "react";
 import { OTP } from "react-custom-otp";
 import "react-custom-otp/dist/index.css";
 
-export default function OtpInputFields({className}) {
-  const [stringCode, setStringCode] = useState("");
-  const [submitStatus, setSubmitStatus] = useState(false);
+export default function OtpInputFields({className, onChange}) {
+    const [submitStatus, setSubmitStatus] = useState(false);
 
-  return (
-    <>
-      <OTP
-        inputsClasses="custom-inputs"
-        inputsStyles={{ background: "", color: "#655445" }}
-        containerClasses="otp-container"
-        containerStyles={{ background: "" }}
-        inputsNumber={6}
-        setStringCode={setStringCode}
-        setSubmitStatus={setSubmitStatus}
-        separator={<span>-</span>}
-        className={className}
-      />
-    </>
-  );
+    return (
+        <>
+            <OTP
+                inputsClasses="custom-inputs"
+                inputsStyles={{ background: "", color: "#655445" }}
+                containerClasses="otp-container"
+                containerStyles={{ background: "" }}
+                inputsNumber={6}
+                setStringCode={onChange}
+                setSubmitStatus={setSubmitStatus}
+                separator={<span>-</span>}
+                className={className}
+            />
+        </>
+    );
 }
