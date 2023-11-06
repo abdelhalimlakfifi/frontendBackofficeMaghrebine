9a44b0d2bfce3cee58bd6dce4053e5ab15a2909c
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-06 23:06:23 +0100

    call sidebar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,31 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 //import components
-import Navbar from "../components/general/navbar"
+import Navbar from "../components/general/navbar";
+import Sidebar from "../components/general/Sidebar";
+import sidebarData from "../components/Sidebar/SidebarData";
 
 export default function Home() {
   const { loading, error, user } = useSelector((state) => state.user);
 
   return (
     <>
-
-      <Navbar/>
-
-      <div className="">
-        {user !== null ? (
-          <div>
-            <Link to="/profile">Profile</Link>
-          </div>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
+      
+      <div className="flex">
+        <Sidebar sidebarData={sidebarData} />
+        <div className="flex flex-col flex-grow">
+           <Navbar/>
+        </div>
+        
+        {/* <div className="">
+          {user !== null ? (
+            <div>
+              <Link to="/profile">Profile</Link>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div> */}
       </div>
     </>
   );
