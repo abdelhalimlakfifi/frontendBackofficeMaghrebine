b8bf7b266d796f45963216414ef788224baa7c2b
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-06 23:07:45 +0100

    export sidebarData

---
+++ src/components/Sidebar/SidebarData.jsx
@@ -0,0 +1,37 @@
+import { CustomersIcon, HomeOutlinedIcon, Permissions, ShoppingCartIcon, StatisticsIcon, HelpOutlineIcon } from "./SidebarIcons"
+
+const sidebarData = [
+    {
+      title: 'Dashboard',
+      items: ['Example'],
+      icon: <HomeOutlinedIcon/>
+    },
+    {
+      title: 'Organization',
+      icon: <Permissions/>,
+      items: ['Roles and Permissions', 'Actions History'],
+    },
+    {
+      title: 'Orders',
+      icon: <ShoppingCartIcon/>,
+      items: ['Orders List', 'Order Details', 'Order Tracking'],
+    },
+    {
+      title: 'Customers',
+      icon: <CustomersIcon/>,
+      items: ['Customer List Page', 'Customer Detail Page', 'Edit Customer Page'],
+    },
+    {
+      title: 'Statistics ',
+      icon: <StatisticsIcon/>,
+      items: ['Search Terms Report', 'Payments and Transactions'],
+    },
+    {
+      title: 'Help Center',
+      icon: <HelpOutlineIcon/>,
+      items: ['General', 'Specific'],
+    },
+  ];
+  
+export default sidebarData;
+  
