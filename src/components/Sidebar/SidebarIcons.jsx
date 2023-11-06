494eed91c61ae9fb0e6be666adecef4d0a62e5de
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-06 23:07:26 +0100

    list sidebar Icons

---
+++ src/components/Sidebar/SidebarIcons.jsx
@@ -0,0 +1,17 @@
+import customers from "../../assets/icons/Customers.svg"
+import HomeOutlined from "../../assets/icons/HomeOutlined.svg"
+import Settings from "../../assets/icons/Settings.svg"
+import ShoppingCart from "../../assets/icons/ShoppingCart.svg"
+import Statistics from "../../assets/icons/Statistics.svg"
+import helpOutline from "../../assets/icons/helpOutline.svg"
+import Permission from "../../assets/icons/permissions.svg"
+
+
+export const CustomersIcon = () => <img src={customers} alt="Customers Icon" />;
+export const HomeOutlinedIcon = () => <img src={HomeOutlined} alt="HomeOutlined Icon" />;
+export const SettingsIcon = () => <img src={Settings} alt="Settings Icon" />;
+export const ShoppingCartIcon = () => <img src={ShoppingCart} alt="ShoppingCart Icon" />;
+export const StatisticsIcon = () => <img src={Statistics} alt="Statistics Icon" />;
+export const HelpOutlineIcon = () => <img src={helpOutline} alt="HelpOutline Icon" />;
+export const Permissions = () => <img src={Permission} alt="User Icon"/>
+
