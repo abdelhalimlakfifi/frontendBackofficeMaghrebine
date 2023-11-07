ffcffced2771f4726f0b0937781a238b7ffc493a
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-07 19:53:18 +0100

    add md:h to NotifButton

--- src/components/navbar/NotifButton.jsx
+++ src/components/navbar/NotifButton.jsx
@@ -6,7 +6,7 @@ const NotifButton = () => {
       <>
         <img
           src={notification}
-          className="h-8"
+          className="h-8 md:h-10"
           alt="notification"
         />
       </>
