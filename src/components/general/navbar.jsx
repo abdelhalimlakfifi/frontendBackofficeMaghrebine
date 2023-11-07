0e1007434e697c81976771f03fc813abafe9e0d7
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-07 19:51:25 +0100

    update navbar style

--- src/components/general/navbar.jsx
+++ src/components/general/navbar.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import { InputText } from "primereact/inputtext";
 import ProfileIcon from "../navbar/ProfileIcon";
 import NotifButton from "../navbar/NotifButton";
-import NavbarLogo from "../navbar/NavbarLogo";
 import MenuIcon from "../navbar/MenuIcon";
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full border-b-2 flex items-center px-3 py-2">
-
-        {/* Menu */}
-        <div className="w-[10%] flex justify-start">
-          <MenuIcon />
-        </div>
-
-        {/* Logo */}
-        <div className="w-[80%] flex justify-center">
-          <NavbarLogo />
-        </div>
+      <div className="w-full h-14 shadow flex items-center px-3 py-2 bg-white  top-0 right-0">
+        {/* search */}
+        {/* <span className="w-[70%] md:w-[90%] flex justify-start">
+          <span className=" text-gray">
+            <InputText
+              id="Search"
+              type="text"
+              placeholder="Search..."
+              className="block w-full  py-1 px-4  text-gray bg-white border focus:border-y-slate-500 rounded-md focus:shadow-xs focus:shadow-gold "
+            />
+          </span>
+        </span> */}
 
         {/* Icons */}
-        <div class="w-[30%] flex justify-end">
-        <div className="flex space-x-5">
+        <div class="w-[100%] md:w-[90%]  flex justify-end">
+          <div className="flex space-x-5">
             <NotifButton />
             <ProfileIcon />
           </div>
