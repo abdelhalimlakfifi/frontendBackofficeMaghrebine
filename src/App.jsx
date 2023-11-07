2a7058a06911beb3b86c6eb9898f7c47216bdfc8
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-07 19:49:38 +0100

    apply font on app

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ResetPassword from './pages/ResetPassword'
 function App() {
 
     return (
-        <>
+        <div className='!font-plusjakarta'>
             <Router>
                 <Routes>
                     <Route exact path='/' element={<Home />} />
@@ -26,7 +26,7 @@ function App() {
                     } />
                 </Routes>
             </Router>
-        </>
+        </div>
     )
 }
 
