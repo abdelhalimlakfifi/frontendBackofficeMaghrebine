a74313b2a897d500532b91106b536a49cd68582f
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-07 13:26:56 +0100

    refactor button

--- src/components/confirmEmail/Button.jsx
+++ src/components/confirmEmail/Button.jsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 import { Button } from "primereact/button";
 
 
-export default function Botton() {
+export default function Botton({ loading }) {
     return (
             <>
-                <div className="w-full  px-4 py-2 my-2 flex justify-center tracking-wide text-white transition-colors duration-200 transform bg-custom-purple rounded-md hover:bg-custom-purple focus:outline-none focus:bg-custom-purple">
-                    <div className="flex items-center ">        
-                        <Button label="Request Password Reset" type="submit" className="text-xm" />
-                    </div>
-                </div>
-                <div  className="flex items-center  ">
-                    <Link to="/Login" className="w-full my-1 underline text-xs flex justify-center text-custom-purple hover:underline">Back To Sign In</Link>
-                </div>
+                <Button 
+                    label="Confirm email" 
+                    type="submit" 
+                    iconPos="right" 
+                    icon="pi pi-check-square" 
+                    className="text-xm w-full bg-custom-purple hover:bg-custom-purple focus:outline-none focus:bg-custom-purple"
+                    loading={loading}
+                />
             </>
     );
 }
