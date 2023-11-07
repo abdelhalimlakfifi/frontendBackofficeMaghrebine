1d2ba8ab6b647cf136814185053f119f05b56be3
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-07 21:05:19 +0100

    import and export icons as components

---
+++ src/components/navbar/NavbarIcons.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//import Icons
+import Calendar from "../../assets/Icons/calendar.svg"
+import Dashboard from "../../assets/Icons/dashboard.svg"
+import Logo from "../../assets/Icons/Logo.svg"
+import Menu from "../../assets/Icons/menu.svg"
+import Notification from "../../assets/Icons/notification.svg"
+import Profile from "../../assets/Icons/ProfileIcon.svg"
+import Search from "../../assets/Icons/Search.svg"
+import ShoppingBag from "../../assets/Icons/ShoppingBag.svg"
+
+//export icon components
+export const Calendar = () => {<img src={Calendar} alt="Calendar" className=""/>}
+export const Dashboard = () => {<img src={Dashboard} alt="Dashboard" className=""/>}
+export const Logo = () => {<img src={Logo} alt="Logo" className=""/>}
+export const Menu = () => {<img src={Menu} alt="Menu" className=""/>}
+export const Notification = () => {<img src={Notification} alt="Notification" className=""/>}
+export const Profile = () => {<img src={Profile} alt="Profile" className=""/>}
+export const Search = () => {<img src={Search} alt="Search" className=""/>}
+export const ShoppingBag = () => {<img src={ShoppingBag} alt="ShoppingBag" className=""/>}
+
