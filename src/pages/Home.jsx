5bf93847ecb7639fda33c21b394cf0b762ea2e36
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-08 11:32:26 +0100

    update components reponsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,34 +5,28 @@ import { useSelector } from "react-redux";
 //import components
 import Sidebar from "../components/general/Sidebar";
 import Navbar from "../components/general/navbar";
-import SearchBar from "../components/navbar/Search"
+import SearchBar from "../components/navbar/Search";
 
 export default function Home() {
-  const { loading, error, user } = useSelector((state) => state.user);
 
   return (
     <div className="flex flex-raw md:flex-col h-screen">
       <div className="flex w-full ">
-        <div className="bg-red-400 w-1/5 hidden lg:block h-screen">
+        <div className="bg-red-400 lg:w-1/5 hidden lg:block h-screen">
           <Sidebar />
         </div>
-        <div className="flex flex-col w-full md:w-4/5 ">
+        <div className="flex flex-col w-full lg:w-4/5 ">
 
-        <div className="shadow-md  h-[8%] flex ">
-          <Navbar />
-        </div>
+            <Navbar />
 
-        <div className=" h-[92%] justify-end mx-3 my-3">
-            <div className="md:hidden">
-            <SearchBar /> 
+          <div className=" h-[92%] justify-end mx-3 my-3">
+            <div className="lg:hidden">
+              <SearchBar />
             </div>
-                     
 
-          <p>This is a component</p>
+            <p>This is a component</p>
+          </div>
         </div>
-
-        </div>
-        
       </div>
     </div>
   );
