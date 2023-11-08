58ea5e26d26df04689072e527c8b458cb44f6233
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-08 16:57:25 +0100

    build sideBar component

--- src/components/general/Sidebar.jsx
+++ src/components/general/Sidebar.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-
+import { SidebarData } from "../sidebar/SidebarData";
+import SidebarSection from "../Sidebar/SidebarSection";
 import LogoIcon from "../../assets/Icons/Logo.svg";
 
-export default function Sidebar ({isOpen}) {
-    return(
-        <>
-            <img src={LogoIcon} alt="Logo" className="" />
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <div>
+        <div className="flex justify-center w-">
+          <img src={LogoIcon} alt="Logo" className="lg:my-5" />
+        </div>
 
-        </>
-    )
+        {SidebarData.map((section, index) => (
+          <SidebarSection section={section} key={index} />
+        ))}
+      </div>
+
+    {/* Logout Button */}
+      <div className="flex items-center justify-between w-auto h-auto cursor-pointer mx-3 my-2 px-2 py-2 text-figma-gray hover:bg-light-gold rounded-lg  hover:text-white hover:duration-200 duration-200 ">
+        <div className="flex items-center space-x-4 mx-2">
+          <div className="w-4">
+            <i className="pi pi-sign-out flex justify-center"></i>
+          </div>
+          <p className="w-[50%]">Logout</p>
+        </div>
+      </div>
+    </div>
+  );
 }
