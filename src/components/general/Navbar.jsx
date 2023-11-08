7391edafe92c6487eee9ed7490a93df1d6504502
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-08 11:31:30 +0100

    update navbar style

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -2,24 +2,38 @@ import React from "react";
 
 import SearchBar from "../navbar/Search";
 import Menu from "../../assets/Icons/Menu.svg";
-import NavbarIcons from "../navbar/NavbarIcons";
+
+//import Icons
+import CalendarIcon from "../../assets/Icons/calendar.svg";
+import DashboardIcon from "../../assets/Icons/dashboard.svg";
+import NotificationIcon from "../../assets/Icons/notification.svg";
+import ProfileIcon from "../../assets/Icons/ProfileIcon.svg";
+import LogoIcon from "../../assets/Icons/Logo.svg";
+
 
 export default function Navbar() {
   return (
-    <div className="flex items-center w-full">
-      <div className="hidden md:block w-[80%] mx-2">
+    <nav className="flex items-center justify-between w-full px-2.5 relative lg:px-6 shadow py-2 md:py-3">
+
+      <div className="hidden lg:block w-full">
         <SearchBar />
       </div>
 
-      <div className="md:hidden w-[8%] flex justify-center">
-        <img src={Menu} alt="Menu" className="" />
+      <img
+        src={Menu}
+        alt="Menu"
+        className="w-8 h-auto lg:hidden cursor-pointer"
+      />
+      <div className="w-full absolute h-full flex items-center justify-center lg:hidden cursor-pointer">
+        <img src={LogoIcon} alt="Logo" className=" md:w-32 h-auto " />
       </div>
-      
-      <div className="flex w-[90%] md:w-[20%] mr-2">
-        <NavbarIcons/>
-      </div>
-      
 
-    </div>
+      <div className="w-2/5 h-full flex justify-end items-center gap-4 lg:gap-8 ">
+        <img src={DashboardIcon} alt="Dashboard" className="hidden lg:block cursor-pointer " />
+        <img src={CalendarIcon} alt="Calendar" className="hidden lg:block cursor-pointer" />
+        <img src={NotificationIcon} alt="Notification" className="cursor-pointer" />
+        <img src={ProfileIcon} alt="Profile" className="cursor-pointer" />
+      </div>
+    </nav>
   );
 }
