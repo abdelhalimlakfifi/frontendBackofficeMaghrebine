f8ff6c756d38b8d2f1f15a5ae25c6186be846bc6
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-08 12:16:56 +0100

    check user if logged only in front

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -2,48 +2,41 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from "react-router-dom";
 import axios from "axios";
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 
 const ProtectedRoute = ({ children }) => {
-    const {user} = useSelector((state) => state);
-    let location = useLocation();
-
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     
     
     useEffect(() => {
 
-        async function checkAuth(){
-            const user  = JSON.parse(localStorage.getItem('user'))
-    
-            try {
-                const response = await axios.get('http://localhost:3000/api/checkAuth',{
-                    headers: {
-                        'Authorization': `Bearer ${user.token}`
-                    }
-                })
-    
-                if(response.status === 200 && response.data.authenticated)
-                {
-                    setIsAuthenticated(true)
-                }else{
-                    setIsAuthenticated(false)
-                }
-                
-            } catch (error) {
-                setIsAuthenticated(false);
-            } finally {
-                setIsLoading(false); // Set loading to false after the request is completed
+        const userLocal  = JSON.parse(localStorage.getItem('user'))
+        // const vare = import.meta.env
+        // console.log(vare);
+        try {
+            if(userLocal){
+                setIsAuthenticated(true)
+            }else{
+                setIsAuthenticated(false)
             }
-            
-        }
 
-        checkAuth();
+            
+        } catch (error) {
+            setIsAuthenticated(false);
+        } finally {
+            setIsLoading(false); // Set loading to false after the request is completed
+        }
 
     },[])
     if (isLoading) {
         // Display a loader or any loading indicator
-        return <div>Loading...</div>;
+        return (
+                <div className="card h-screen flex items-center">
+                    <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />
+                </div>
+        )
     }
 
     if (!isAuthenticated) {
