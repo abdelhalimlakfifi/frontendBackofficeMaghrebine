c9ef304e18beff83148ed3e3045468d48ff4100f
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-09 17:21:38 +0100

    adjust section style

--- src/components/sidebar/SidebarSection.jsx
+++ src/components/sidebar/SidebarSection.jsx
@@ -18,7 +18,7 @@ function SidebarSection({ section }) {
             <Link 
                 to={section.link}
                 key={section}
-                className={`flex items-center justify-between w-auto h-auto mx-3 my-2 px-2 py-2 text-figma-gray hover:bg-light-gold rounded-lg  hover:text-white hover:duration-200 duration-200 ${
+                className={`flex items-center justify-between w-auto h-auto mx-3 md:my-7  lg:my-2 px-2 py-2 text-figma-gray hover:bg-light-gold rounded-lg  hover:text-white hover:duration-200 duration-200 ${
                 toggle && "bg-light-gold text-white"
                 } `}
             >
@@ -42,7 +42,7 @@ function SidebarSection({ section }) {
             toggle && "bg-light-gold text-white"
             } `}
         >
-            <div className="flex items-center space-x-4 mx-2">
+            <div className="flex items-center space-x-4 mx-2 ">
             <div className="w-4">
                 <i className={section.icon}></i>
                     
@@ -62,12 +62,12 @@ function SidebarSection({ section }) {
       {section.items.length > 0 ? (
             section.items.map((item, index) => (
                 toggle && (
-                    <Link to={item.link} key={index} className="flex items-center justify-between w-auto h-auto  mx-3 my-2 px-2 py-2 hover:text-light-gold rounded-lg  text-figma-gray hover:duration-200 duration-200">
+                    <Link to={item.link} key={index} className="flex items-center justify-between w-auto h-auto  mx-3 my-1 md:my-2 lg:my-2 px-2 py-1 lg:py-1 hover:text-light-gold rounded-lg  text-figma-gray hover:duration-200 duration-200">
                       <div className="flex items-center space-x-4 mx-2">
                         <div className="w-4">
-                          <i className="pi pi-circle-fill text-[0.45rem]  "></i>
+                          <i className="pi pi-circle-fill text-[0.30rem]  "></i>
                         </div>
-                        <p className="w-full">{item.title}</p>
+                        <p className="w-full text-xs md:text-xs">{item.title}</p>
                       </div>
                     </Link>
                   )
