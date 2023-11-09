fc2ef746c86f9c7c8024d49879561f17be0b76ba
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-09 17:22:37 +0100

    handle isOpen state with two actions handleMenuClick and closeSidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 //import components
 import Sidebar from "../components/general/Sidebar";
-import Navbar from "../components/general/navbar";
+import Navbar from "../components/general/Navbar";
 import SearchBar from "../components/navbar/Search";
+import { Button } from "primereact/button";
 
 export default function Home() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function handleMenuClick () {
+        setIsOpen(!isOpen)
+  }
+
+  function closeSidebar () {
+     setIsOpen(false)
+  }
 
   return (
     <div className="flex flex-raw md:flex-col h-screen">
-      <div className="flex w-full ">
-        <div className="shadow lg:w-1/5 hidden lg:block h-screen">
-          <Sidebar/>
-        </div>
-        <div className="flex flex-col w-full lg:w-4/5 ">
+      <div className="flex w-full  ">
+          <Sidebar isOpen={isOpen} />
+        <div className="flex flex-col w-full fixed lg:static lg:w-4/5 ">
 
-            <Navbar />
+          {isOpen && (
+            <div className="w-full h-screen absolute bg-black opacity-80 z-50 duration-500 ease-in-out" onClick={closeSidebar}>
+             
+            </div>
+          )}
+          <div className="z-10">
+            <Navbar handleMenuClick={handleMenuClick} />
 
           <div className=" h-[92%] justify-end mx-3 my-3">
             <div className="lg:hidden">
-              <SearchBar />
+              <SearchBar/>
             </div>
 
             <p>This is a component</p>
+          </div>
           </div>
         </div>
       </div>
