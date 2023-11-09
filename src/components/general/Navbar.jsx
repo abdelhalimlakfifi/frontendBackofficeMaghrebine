d06aad0444f9c627c7d273e2557a1167ab8516fd
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-09 17:18:17 +0100

    update icons and add hover style on them

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -4,22 +4,14 @@ import SearchBar from "../navbar/Search";
 import Menu from "../../assets/Icons/Menu.svg";
 
 //import Icons
-import CalendarIcon from "../../assets/Icons/calendar.svg";
-import DashboardIcon from "../../assets/Icons/dashboard.svg";
-import NotificationIcon from "../../assets/Icons/notification.svg";
+
 import ProfileIcon from "../../assets/Icons/ProfileIcon.svg";
 import LogoIcon from "../../assets/Icons/Logo.svg";
 
+import { Button } from 'primereact/button';
+        
 
-export default function Navbar() {
-  
-  // const [isOpen, setIsOpen] = useState(false);
-
-  const handleMenuClick = (e) => {
-    e.preventDefault();
-  
-  }
-
+export default function Navbar({handleMenuClick}) {
 
   return (
     <nav className="flex items-center justify-between w-full px-2.5 relative lg:px-6 shadow py-2 md:py-3">
@@ -27,24 +19,29 @@ export default function Navbar() {
       <div className="hidden lg:block w-full">
         <SearchBar />
       </div>
+            
 
-      <img
-        src={Menu}
-        alt="Menu"
-        className="w-8 h-auto lg:hidden cursor-pointer"
-        onClick={handleMenuClick}
-    
+      <button onClick={handleMenuClick} >
+        <img
+          src={Menu}
+          alt="Menu"
+          className="w-8 h-auto lg:hidden "
+          
+        />
+      </button>
 
-      />
-      <div className="w-full absolute h-full flex items-center justify-center lg:hidden cursor-pointer">
+      <div className="w-full h-full flex items-center justify-center lg:hidden cursor-pointer">
         <img src={LogoIcon} alt="Logo" className=" md:w-32 h-auto " />
       </div>
 
-      <div className="w-2/5 h-full flex justify-end items-center gap-4 lg:gap-8 ">
-        <img src={DashboardIcon} alt="Dashboard" className="hidden lg:block cursor-pointer " />
+      <div className="w-2/5 h-full flex justify-end items-center gap-4 lg:gap-8 text-figma-gray">
+      <i className="pi pi-calendar hidden lg:block cursor-pointer hover:text-light-gold" alt="Dashboard"></i>
+      <i className="pi pi-th-large hidden lg:block cursor-pointer hover:text-light-gold" alt="Calendar"></i>
+      <i className="pi pi-bell cursor-pointer hover:text-light-gold " alt="Notification"></i>
+        {/* <img src={DashboardIcon} alt="Dashboard" className="hidden lg:block cursor-pointer " />
         <img src={CalendarIcon} alt="Calendar" className="hidden lg:block cursor-pointer" />
-        <img src={NotificationIcon} alt="Notification" className="cursor-pointer" />
-        <img src={ProfileIcon} alt="Profile" className="cursor-pointer" />
+        <img src={NotificationIcon} alt="Notification" className="cursor-pointer" /> */}
+        <img src={ProfileIcon} alt="Profile" className="cursor-pointer " />
       </div>
     </nav>
   );
