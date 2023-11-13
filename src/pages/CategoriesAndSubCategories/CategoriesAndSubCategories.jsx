a08b7ccb2ab8a563fb1add2658b72f073814b87c
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-14 00:54:30 +0100

    build panel components

--- src/pages/CategoriesAndSubCategories/CategoriesAndSubCategories.jsx
+++ src/pages/CategoriesAndSubCategories/CategoriesAndSubCategories.jsx
@@ -1,27 +1,48 @@
-import React, {useState,useEffect} from 'react'
-import Layout from '../../layouts/layouts'
-import { Tree } from 'primereact/tree';
-import { Categories } from './categories';
+import React from "react";
+import Layout from "../../layouts/layouts";
+import TypesTable from "./TypesTable";
+import { TabView, TabPanel } from "primereact/tabview";
+import { Toolbar } from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+import CrudTypes from "./crudTypes";
+
+
 
 export default function CategoriesAndSubCategories() {
-    const [nodes, setNodes] = useState([{
-        label: "Rbati",
-        icon: "pi pi-star-fill",
-        children: [{
-            label: 'Work',
-        }]
-    }]);
 
-    useEffect(() => {
-        // Categories.getTreeNodes().then((data) => setNodes(data));
-    }, []);
-    return (
-        <Layout>
-            <div>
-                <span>Categorie</span>
+    // const leftToolbarTemplate = () => {
+    //     return (
+    //         <div className="flex flex-wrap gap-2 ">
+    //             <Button label="Delete" icon="pi pi-trash" severity="danger" text raised/>
+    //         </div>
+    //     );
+    // };
 
-                <Tree value={nodes} dragdropScope="demo" onDragDrop={(e) => setNodes(e.value)} className="w-full md:w-30rem" />
-            </div>
-        </Layout>
-    )
+    const rightToolbarTemplate = () => {
+        return (
+            <>
+                <CrudTypes/>
+                <Button label="Export" icon="pi pi-upload" className="p-button-help  mx-2" text />
+            </>
+        )
+        
+    };
+    
+
+  return (
+    <Layout>
+      <TabView className="px-20 py-8 text-xs ">
+        <TabPanel header="Types">
+            <Toolbar className="mb-4 " right={rightToolbarTemplate}></Toolbar>
+            <TypesTable/>
+        </TabPanel>
+        <TabPanel header="Categories">
+          <p className="m-0"></p>
+        </TabPanel>
+        <TabPanel header="Sub-Categories">
+          <p className="m-0"></p>
+        </TabPanel>
+      </TabView>
+    </Layout>
+  );
 }
