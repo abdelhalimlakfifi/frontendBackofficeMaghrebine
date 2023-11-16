4d978a2566020a3960470c65fc4a9bfb1e70f02d
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-16 15:35:23 +0100

    product store page

---
+++ src/pages/products/ProductStore.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../layouts/layouts'
+export default function ProductStore() {
+    return (
+
+        <Layout>
+            <div>Store</div>
+        </Layout>
+    )
+}
