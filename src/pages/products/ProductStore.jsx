81ef9bae865a68c68f6e73ed400b7ea6d07f2500
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-17 10:22:36 +0100

    make a condition for spet on store

--- src/pages/products/ProductStore.jsx
+++ src/pages/products/ProductStore.jsx
@@ -1,75 +1,78 @@
-import React, { useState, useRef} from 'react'
-import Layout from '../../layouts/layouts'
+import React, { useState, useEffect } from 'react';
+import Layout from '../../layouts/layouts';
 import { Steps } from 'primereact/steps';
 import InformationsForm from './ProductComponents/InformationsForm';
+import ColorsAndImageForm from './ProductComponents/ColorsAndImageForm';
+import FiltersStoreForm from './ProductComponents/FiltersStoreForm';
 import { Button } from 'primereact/button';
 
 export default function ProductStore() {
     const [activeIndex, setActiveIndex] = useState(0);
-    const [data, setData] = useState({});
 
     const items = [
-        {
-            label: 'Information',
-            command: (event) => {
-                
-            }
-        },
-        {
-            label: 'Colors&images',
-            command: (event) => {
-                
-            }
-        },
-        {
-            label: 'Filters',
-            command: (event) => {
-                
-            }
-        },
-        
+        { label: 'Information', command: () => {} },
+        { label: 'Colors&images', command: () => {} },
+        { label: 'Filters', command: () => {} },
     ];
+
+    const handlePrevious = () => setActiveIndex(activeIndex - 1);
+    const handleNext = () => setActiveIndex(activeIndex + 1);
+
+    const isPreviousDisabled = activeIndex === 0;
+    const isNextDisabled = activeIndex === items.length - 1;
+
+    const stepStyles = {
+        step: { className: 'w-12 h-12 text-lg' },
+        activeStep: { className: 'font-bold' },
+    };
+
+    useEffect(() => {
+        console.log(activeIndex);
+    }, [activeIndex]);
+
+    const stepComponents = [InformationsForm, ColorsAndImageForm, FiltersStoreForm];
+
     return (
-
         <Layout>
+            <div className='mx-4 lg:mx-32 mt-8 min-h-[80vh] md:min-h-[88vh] flex flex-col justify-between'>
 
-            <div className=' mx-4 lg:mx-32 mt-8'>
-                <Steps 
-                    model={items}
-                    activeIndex={activeIndex}
-                    onSelect={(e) => setActiveIndex(e.index)}
-                    readOnly={false}
-                    pt={{
-                        step: { className: 'w-12 h-12 text-lg'},
-                        activeStep: { className: 'font-bold' }
-                    }}
-                />
+                <div>
+                    <Steps
+                        model={items}
+                        activeIndex={activeIndex}
+                        onSelect={(e) => setActiveIndex(e.index)}
+                        readOnly={false}
+                        pt={stepStyles}
+                    />
 
-                {activeIndex === 0 && (
                     <div>
-                        <InformationsForm />
+                        {stepComponents.map((FormComponent, index) => (
+                            <div key={index} style={{ display: index === activeIndex ? 'block' : 'none' }}>
+                                <FormComponent />
+                            </div>
+                        ))}
                     </div>
-                )}
 
+                </div>
 
-                {activeIndex === 1 && (
-                    <div>
-                        <InformationsForm />
-                    </div>
-                )}
-                {activeIndex === 2 && (
-                    <div>
-                        <InformationsForm />
-                    </div>
-                )}
-
-                <div className='w-full flex justify-between'>
-
-                    <Button disabled={activeIndex == 0} onClick={() => setActiveIndex(activeIndex - 1)} label="Previous" icon="pi pi-angle-left" className=' bg-light-gold border-light-gold'/>
-                    <Button disabled={activeIndex == items.length - 1} onClick={() => setActiveIndex(activeIndex + 1)} label="Next" iconPos="right" icon="pi pi-angle-right" className=' bg-light-gold border-light-gold' />
+                <div className='w-full flex justify-between bottom-4'>
+                    <Button
+                        disabled={isPreviousDisabled}
+                        onClick={handlePrevious}
+                        label='Previous'
+                        icon='pi pi-angle-left'
+                        className='bg-light-gold border-light-gold'
+                    />
+                    <Button
+                        disabled={isNextDisabled}
+                        onClick={handleNext}
+                        label='Next'
+                        iconPos='right'
+                        icon='pi pi-angle-right'
+                        className='bg-light-gold border-light-gold'
+                    />
                 </div>
             </div>
-
         </Layout>
-    )
+    );
 }
