3ac2ace114540fa1c32f148ace37318bb9cd64a5
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-17 22:48:10 +0100

    make filters srore file

---
+++ src/pages/products/ProductComponents/FiltersStoreForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react'
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from 'primereact/inputnumber';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Editor } from 'primereact/editor';
+
+export default function FiltersStoreForm() {
+    const [price, setPrice] = useState(0)
+    return (
+        <div className=' my-2 flex flex-col space-y-6'>
+            
+
+
+
+        </div>
+    )
+}
