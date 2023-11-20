7afe5f1d88c1404943c9ed994f08f78a9b2c1555
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-11-20 18:36:14 +0100

    fix import files

--- src/pages/CategoriesAndSubCategories/CategoriesAndSubCategories.jsx
+++ src/pages/CategoriesAndSubCategories/CategoriesAndSubCategories.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Layout from "../../layouts/layouts";
 import { TabView, TabPanel } from "primereact/tabview";
-import { Toolbar } from 'primereact/toolbar';
 
+import TypesTable from "./TypesTable";
+import CategoriesTable from "./CategoriesTable";
+import SubCategoriesTable from "./SubCategoriesTable";
 
 export default function CategoriesAndSubCategories() {
-    
   return (
     <Layout>
       <TabView className="px-20 py-8 text-xs ">
         <TabPanel header="Types">
-            <Toolbar className="mb-4 " right={rightToolbarTemplate}></Toolbar>
-            {/* <TypesTable/> */}
+          <TypesTable />
         </TabPanel>
         <TabPanel header="Categories">
-          
+          <CategoriesTable />
         </TabPanel>
         <TabPanel header="Sub-Categories">
-          
+          <SubCategoriesTable />
         </TabPanel>
       </TabView>
     </Layout>
