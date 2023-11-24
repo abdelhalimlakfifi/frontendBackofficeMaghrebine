2b6bbf3fe68bffbc7c363710631c60c76e166fd1
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-24 12:32:22 +0100

    make user Data

---
+++ src/pages/Users/userData/user.data.js
@@ -0,0 +1,18 @@
+export const UsersServices = {
+    getUsersData() {
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+
+
+    getAllUsers() {
+        return Promise.resolve(this.getProductsData().slice(0, 5));
+    },
+
+    
+};
+
