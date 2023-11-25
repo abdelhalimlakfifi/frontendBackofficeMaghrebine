0475a433f59dd6fac3fed5018fdd1a46fcbfa1ba
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-25 22:17:21 +0100

    make the interface for users

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,34 +1,118 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 import { UsersServices } from "./userData/user.data";
 import Layout from "../../layouts/layouts";
-
-
+import { useNavigate } from "react-router";
+import { Avatar } from 'primereact/avatar';
+import { Tooltip } from 'primereact/tooltip';
+import { BreadCrumb } from 'primereact/breadcrumb';
+import { Link } from "react-router-dom";
 function Users() {
-  const [products, setProducts] = useState([]);
-  const columns = [
-    { field: "code", header: "Code" },
-    { field: "name", header: "Name" },
-    { field: "category", header: "Category" },
-    { field: "quantity", header: "Quantity" },
-  ];
+    const [users, setUsers] = useState([]);
+    const [displayedUsers, setDisplayedUsers] = useState([]);
+    const items = [{ label: 'Users' }];
+    const home = { icon: 'pi pi-home', url: '/' }
 
-  useEffect(() => {
-    console.log(UsersServices)
-  }, []);
+    const navigate = useNavigate();
 
-  return (
-    <Layout>
-      <div className="card">
-        <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
-          {columns.map((col, i) => (
-            <Column key={col.field} field={col.field} header={col.header} />
-          ))}
-        </DataTable>
-      </div>
-    </Layout>
-  );
+    useEffect(() => {
+        console.clear(); // eslint-disable-line no-console
+
+        const unauthorizedCallback = () => {
+            alert("Unauthorized access! Redirecting to login.");
+            navigate("/login");
+        };
+        const getUsers = async () => {
+            const data = await UsersServices.getAllUsers(unauthorizedCallback);
+            setUsers(data);
+            setDisplayedUsers(data);
+        };
+        getUsers();
+    }, []);
+
+    useEffect(() => {
+        console.log(users);
+    }, [users]);
+
+    const searchable = (e) => {
+        const searchTerm = e.target.value.toLowerCase();
+    
+        // Use setDisplayedUsers to update the state with the filtered array
+        setDisplayedUsers(users.filter(user => {
+            const firstName = user.first_name.toLowerCase();
+            const lastName = user.last_name.toLowerCase();
+            const username = user.username.toLowerCase();
+
+            return (
+                firstName.includes(searchTerm) ||
+                lastName.includes(searchTerm) ||
+                username.includes(searchTerm)
+            );
+        }));
+    }
+
+    const header = (
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <Link to="/add-user">
+                <Button label="New user" className=" text-sm bg-light-gold border-none" icon="pi pi-user-plus" />
+            </Link>
+            <span>
+                <i className="pi pi-search" style={{ margin: "4px 4px 0 0" }}></i>
+                <InputText
+                    type="search"
+                    onInput={searchable}
+                    placeholder="Search"
+                />
+            </span>
+        </div>
+    );
+
+    const fullname = (row) => {
+        return (
+            <div className="flex space-x-2">
+                <Avatar label={row.last_name.toUpperCase()[0]} size="small" shape="circle" className="hidden md:inline-flex" />
+                <p className="flex items-center">
+                    {`${row.last_name} ${row.first_name}`}
+                </p>
+            </div>
+        )
+    }
+
+    const actions = (row) => {
+        return (
+            <div className="flex space-x-4">
+                <Button label="Edit" className=" text-sm bg-light-gold border-none" icon="pi pi-user-edit"  />
+                <Button icon="pi pi-trash" className="bg-transparent border-light-gold text-light-gold" tooltip="Delete user" tooltipOptions={{  position: 'top' }}/>
+            </div>
+        )
+    }
+    return (
+        <Layout>
+            <BreadCrumb model={items} home={home} />
+
+            <div className="card">
+                <DataTable
+                    value={displayedUsers}
+                    paginator
+                    rows={5}
+                    rowsPerPageOptions={[5, 10, 25, 50]}
+                    header={header}
+                    scrollable
+                    showGridlines
+                    stripedRows 
+                >
+                    <Column field={"last_name"} header="Full name" sortable body={(row) => fullname(row)} />
+                    <Column field={"username"} header="Username" sortable />
+                    <Column field={"email"} header="email" sortable />
+                    <Column field={"role.id"} header="Role" sortable />
+                    <Column header="Actions"  body={(row) => actions(row)} />
+                </DataTable>
+            </div>
+        </Layout>
+    );
 }
 
 export default Users;
