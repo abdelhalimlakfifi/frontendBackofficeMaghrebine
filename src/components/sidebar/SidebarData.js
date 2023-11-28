13b59f9064ce3fee0c7daa76425ecf4aed596a6d
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-28 21:54:45 +0100

    refactor code

--- src/components/sidebar/SidebarData.js
+++ src/components/sidebar/SidebarData.js
@@ -1,47 +1,71 @@
-
-export const SidebarData = [
-    {
-      title: 'Dashboard',
-      link: '/dashboard',
-      icon: "pi pi-home flex justify-center",
-      items: [],
+export const SidebarData = [{
+        title: 'Dashboard',
+        link: '/dashboard',
+        icon: "pi pi-home flex justify-center",
+        items: [],
     },
     {
-      title: 'Categories',
-      link: '/categories-subcategories',
-      icon: "pi pi-lock flex justify-center",
-      // items: [{title:'Roles and Permissions', link:'/Roles-Permissions'}, {title:'Actions Log', link:'/Actions-Log'}],
-      items: [],
+        title: 'Categories',
+        link: '/categories-subcategories',
+        icon: "pi pi-lock flex justify-center",
+        // items: [{title:'Roles and Permissions', link:'/Roles-Permissions'}, {title:'Actions Log', link:'/Actions-Log'}],
+        items: [],
     },
     {
-      title: 'Types',
-      link: '/types',
-      icon: "pi pi-shopping-bag  flex justify-center",
-      items: [{title:'Types List', link:'/categories-subcategories'}],
+        title: 'Types',
+        link: '/types',
+        icon: "pi pi-shopping-bag  flex justify-center",
+        items: [{
+            title: 'Types List',
+            link: '/categories-subcategories'
+        }],
     },
     {
-      title: 'Roles',
-      link: '/roles',
-      icon: "pi pi-shopping-bag  flex justify-center",
-      items: [{title:'Roles List', link:'/roles'}],
+        title: 'Roles',
+        link: '/roles',
+        icon: "pi pi-shopping-bag  flex justify-center",
+        items: [{
+            title: 'Roles List',
+            link: '/roles'
+        }],
     },
     {
-      title: 'Customers',
-      link: '/Customers',
-      icon: "pi pi-users flex justify-center",
-      items: [{title:'Customers List', link:'/Customers-List'}],
+        title: 'Customers',
+        link: '/Customers',
+        icon: "pi pi-users flex justify-center",
+        items: [{
+            title: 'Customers List',
+            link: '/Customers-List'
+        }],
     },
     {
-      title: 'Statistics ',
-      link: '/Statistics',
-      icon: "pi pi-chart-bar flex justify-center",
-      items: [{title:'Reports List', link:'/Reports'}, {title:'Transactions', link:'/Transactions'}],
+        title: 'Users',
+        link: '/users',
+        icon: "pi pi-users flex justify-center",
+        items: [{
+            title: 'User List',
+            link: '/users'
+        },{
+            title: 'Add User',
+            link: '/add-user'
+        }],
     },
     {
-      title: 'Help Center',
-      link: '/Help-Center', // or '/HelpCenter'
-      icon: "pi pi-shopping-bag  flex justify-center",
-      items: [],
+        title: 'Statistics ',
+        link: '/Statistics',
+        icon: "pi pi-chart-bar flex justify-center",
+        items: [{
+            title: 'Reports List',
+            link: '/Reports'
+        }, {
+            title: 'Transactions',
+            link: '/Transactions'
+        }],
+    },
+    {
+        title: 'Help Center',
+        link: '/Help-Center', // or '/HelpCenter'
+        icon: "pi pi-shopping-bag  flex justify-center",
+        items: [],
     }
-  ];
-  
+];
