dd1dd70256a935b97daf70c5611143ab8090f0a6
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-29 11:09:24 +0100

    refactore Code

--- src/utils/request.js
+++ src/utils/request.js
@@ -12,12 +12,6 @@ export async function get(url, token, unauthorizedCallback ,...others){
         return response.data;
     } catch (error) {
         if (error.response.status === 401) {
-            // if(error.response.data.error === 'Token has expired')
-            // {
-            //   alert('Token has expired');
-            // }else{
-            //   alert('Token is invalid')
-            // }
             unauthorizedCallback()
         }
         return error.data;
