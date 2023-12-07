54277e77efb229fa252ae29db0902ff1b93f962d
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-12-07 13:30:48 +0100

    refactore code and make Product lists

--- src/components/sidebar/SidebarData.js
+++ src/components/sidebar/SidebarData.js
@@ -1,41 +1,62 @@
-
-export const SidebarData = [
-    {
-      title: 'Dashboard',
-      link: '/dashboard',
-      icon: "pi pi-home flex justify-center",
-      items: [],
+export const SidebarData = [{
+        title: 'Dashboard',
+        link: '/dashboard',
+        icon: "pi pi-home flex justify-center",
+        items: [],
     },
     {
-      title: 'Categories',
-      link: '/categories-subcategories',
-      icon: "pi pi-lock flex justify-center",
-      // items: [{title:'Roles and Permissions', link:'/Roles-Permissions'}, {title:'Actions Log', link:'/Actions-Log'}],
-      items: [],
+        title: 'Categories',
+        link: '/categories-subcategories',
+        icon: "pi pi-lock flex justify-center",
+        // items: [{title:'Roles and Permissions', link:'/Roles-Permissions'}, {title:'Actions Log', link:'/Actions-Log'}],
+        items: [],
     },
     {
-      title: 'Types',
-      link: '/types',
-      icon: "pi pi-shopping-bag  flex justify-center",
-      items: [],
+        title: 'Types',
+        link: '/types',
+        icon: "pi pi-shopping-bag  flex justify-center",
+        items: [],
     },
     {
-      title: 'Customers',
-      link: '/Customers',
-      icon: "pi pi-users flex justify-center",
-      items: [{title:'Customers List', link:'/Customers-List'}],
+        title: 'Customers',
+        link: '/Customers',
+        icon: "pi pi-users flex justify-center",
+        items: [{
+            title: 'Customers List',
+            link: '/Customers-List'
+        }],
     },
     {
-      title: 'Statistics ',
-      link: '/Statistics',
-      icon: "pi pi-chart-bar flex justify-center",
-      items: [{title:'Reports List', link:'/Reports'}, {title:'Transactions', link:'/Transactions'}],
+        title: 'Products',
+        link: '/products',
+        icon: 'pi pi-box flex justify-center',
+        items: [
+            {
+                title: 'Products List',
+                link: '/products',
+            },
+            {
+                title: 'New Product',
+                link: '/product-store',
+            }
+        ]
     },
     {
-      title: 'Help Center',
-      link: '/Help-Center', // or '/HelpCenter'
-      icon: "pi pi-shopping-bag  flex justify-center",
-      items: [],
+        title: 'Statistics ',
+        link: '/Statistics',
+        icon: "pi pi-chart-bar flex justify-center",
+        items: [{
+            title: 'Reports List',
+            link: '/Reports'
+        }, {
+            title: 'Transactions',
+            link: '/Transactions'
+        }],
+    },
+    {
+        title: 'Help Center',
+        link: '/Help-Center', // or '/HelpCenter'
+        icon: "pi pi-shopping-bag  flex justify-center",
+        items: [],
     }
-  ];
-  
+];
