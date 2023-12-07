233193df4c9c9092be65c8a9ff1c92b93c227103
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-12-07 13:31:27 +0100

    create new component for Product list

---
+++ src/pages/products/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../layouts/layouts';
+
+
+export default function ProductList() 
+{
+    return (
+        <Layout>
+            Product List;
+        </Layout>
+    )
+}
