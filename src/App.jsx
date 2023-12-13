0838f416b3bfb7357edf0153a1ec2760c6f57741
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-12-13 10:35:37 +0100

    pretier

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
@@ -16,82 +15,80 @@ import Roles from "./pages/Roles/Roles";
 import AddUser from "./pages/Users/AddUser";
 import EditUser from "./pages/Users/EditUser";
 
-
-
 // import Categories from './pages/CategoriesAndSubCategories/Categories'
-import ProductStore from './pages/products/ProductStore';
-import ProductList from './pages/products/ProductList'
+import ProductStore from "./pages/products/ProductStore";
+import ProductList from "./pages/products/ProductList";
 
 import Customers from "./pages/Customers/Customers";
 import InfoCustomer from "./pages/Customers/InfoCustomer";
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                {/* session-expired */}
-                    <Route
-                        path="/session-expired"
-                        element={
-                            <GuestRoutes>
-                                <h1>Hello from session Expired</h1>
-                            </GuestRoutes>
-                        }
-                    />
-                    <Route
-                        path="/unauthorized"
-                        element={
-                            <GuestRoutes>
-                                <h1>Hello from Unauthorized</h1>
-                            </GuestRoutes>
-                        }
-                    />
-                    <Route
-                        path="/login"
-                        element={
-                            <GuestRoutes>
-                                <Login />
-                            </GuestRoutes>
-                        }
-                    />
-                    <Route path="/ConfirmEmail" element={<ConfirmEmail />} />
-                    <Route
-                        path="/InsertOtp"
-                        element={
-                            <GuestRoutes>
-                                <InsertOtp />
-                            </GuestRoutes>
-                        }
-                    />
-                    <Route
-                        path="/ResetPassword"
-                        element={
-                            <GuestRoutes>
-                                <ResetPassword />
-                            </GuestRoutes>
-                        }
-                    />
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* session-expired */}
+          <Route
+            path="/session-expired"
+            element={
+              <GuestRoutes>
+                <h1>Hello from session Expired</h1>
+              </GuestRoutes>
+            }
+          />
+          <Route
+            path="/unauthorized"
+            element={
+              <GuestRoutes>
+                <h1>Hello from Unauthorized</h1>
+              </GuestRoutes>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <GuestRoutes>
+                <Login />
+              </GuestRoutes>
+            }
+          />
+          <Route path="/ConfirmEmail" element={<ConfirmEmail />} />
+          <Route
+            path="/InsertOtp"
+            element={
+              <GuestRoutes>
+                <InsertOtp />
+              </GuestRoutes>
+            }
+          />
+          <Route
+            path="/ResetPassword"
+            element={
+              <GuestRoutes>
+                <ResetPassword />
+              </GuestRoutes>
+            }
+          />
 
-                {/* authenticated */}
-                    <Route
-                        exact
-                        path="/"
-                        element={
-                            <ProtectedRoute>
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
-                    {/* Users */}
-                    <Route
-                        path="/users"
-                        element={
-                            <ProtectedRoute>
-                                <Users />
-                            </ProtectedRoute>
-                        }
-                    />
+          {/* authenticated */}
+          <Route
+            exact
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          {/* Users */}
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>
+            }
+          />
           <Route
             path="/customers"
             element={
@@ -118,81 +115,90 @@ function App() {
           />
 
           <Route path="/roles" element={<Roles />} />
-                    <Route 
-                        path="/add-user"
-                        element={
-                            <ProtectedRoute>
-                                <AddUser />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/edit-user"
-                        element={
-                            <ProtectedRoute>
-                                <EditUser />
-                            </ProtectedRoute>
-                        }
-                    />
-                    {/* users */}
-                    <Route
-                        path="/filters"
-                        element={
-                            <ProtectedRoute>
-                                <CategoriesAndSubCategories />
-                            </ProtectedRoute>
-                        }
-                    />
+          <Route
+            path="/add-user"
+            element={
+              <ProtectedRoute>
+                <AddUser />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit-user"
+            element={
+              <ProtectedRoute>
+                <EditUser />
+              </ProtectedRoute>
+            }
+          />
+          {/* users */}
+          <Route
+            path="/filters"
+            element={
+              <ProtectedRoute>
+                <CategoriesAndSubCategories />
+              </ProtectedRoute>
+            }
+          />
 
           <Route path="/roles" element={<Roles />} />
 
-                    <Route
-                        path="/types"
-                        element={
-                            <ProtectedRoute>
-                                <Types />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="/profile"
-                        element={
-                            <ProtectedRoute>
-                                <Profile />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route path='/types' element={
-                        <ProtectedRoute>
-                            <Types />
-                        </ProtectedRoute>
-                    } />
+          <Route
+            path="/types"
+            element={
+              <ProtectedRoute>
+                <Types />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/types"
+            element={
+              <ProtectedRoute>
+                <Types />
+              </ProtectedRoute>
+            }
+          />
 
-                    {/* ProductList */}
-                    <Route
-                        path='products'
-                        element={
-                            <ProtectedRoute>
-                                <ProductList />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route path='/product-store' element={
-                        <ProtectedRoute>
-                            <ProductStore />
-                        </ProtectedRoute>
-                    } />
+          {/* ProductList */}
+          <Route
+            path="products"
+            element={
+              <ProtectedRoute>
+                <ProductList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/product-store"
+            element={
+              <ProtectedRoute>
+                <ProductStore />
+              </ProtectedRoute>
+            }
+          />
 
-                    <Route path='/profile' element={
-                        <ProtectedRoute>
-                            <Profile/>
-                        </ProtectedRoute>
-                    } />
-                    {/* authenticated */}
-                </Routes>
-            </Router>
-        </>
-    );
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          {/* authenticated */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
