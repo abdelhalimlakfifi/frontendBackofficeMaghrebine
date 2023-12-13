63275b3ff4b1e21bf0fabff643e52fd63f4d2368
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-12-13 10:35:56 +0100

    set the dashbord sidebar

--- src/components/sidebar/SidebarData.js
+++ src/components/sidebar/SidebarData.js
@@ -1,43 +1,49 @@
 export const SidebarData = [
   {
-    title: 'Dashboard',
-    link: '/',
+    title: "Dashboard",
+    link: "/",
     icon: "pi pi-home flex justify-center",
     items: [],
   },
   {
-    title: 'Filters',
-    link: '/filters',
+    title: "Filters",
+    link: "/filters",
     icon: "pi pi-lock flex justify-center",
     // items: [{title:'Roles and Permissions', link:'/Roles-Permissions'}, {title:'Actions Log', link:'/Actions-Log'}],
     items: [],
   },
   {
-    title: 'Roles',
-    link: '/roles',
+    title: "Roles",
+    link: "/roles",
     icon: "pi pi-shopping-bag  flex justify-center",
-    // items: [{title:'Roles List', link:'/roles'}],
     items: [],
   },
   {
-    title: 'Customers',
-    link: '/Customers',
+    title: "Product Store",
+    link: "/product-store",
     icon: "pi pi-users flex justify-center",
-    items: [{title:'Customers List', link:'/Customers-List'}],
+    items: [],
   },
   {
-    title: 'Statistics ',
-    link: '/Statistics',
+    title: "Users",
+    link: "/users",
+    icon: "pi pi-users flex justify-center",
+    items: [],
+  },
+
+  {
+    title: "Statistics ",
+    link: "/Statistics",
     icon: "pi pi-chart-bar flex justify-center",
-    items: [{title:'Reports List', link:'/Reports'}, {title:'Transactions', link:'/Transactions'}],
+    items: [
+      { title: "Reports List", link: "/Reports" },
+      { title: "Transactions", link: "/Transactions" },
+    ],
   },
   {
-    title: 'Help Center',
-    link: '/Help-Center', // or '/HelpCenter'
+    title: "Help Center",
+    link: "/Help-Center", // or '/HelpCenter'
     icon: "pi pi-shopping-bag  flex justify-center",
     items: [],
-  }
+  },
 ];
-
-
-
